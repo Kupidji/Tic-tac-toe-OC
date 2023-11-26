c3b3093b28b1c54d2e631e1b6f976211e4db9bdd
Kirill <kirill_koksharov@list.ru>
2023-11-26 14:17:29 +0500

    Поправил 5 лабу. Теперь при запуске приложения идет проверка на существования в памяти игрового поля. Почистил код

--- Main.cpp
+++ Main.cpp
@@ -9,12 +9,16 @@ auto APP_NAME = L"Крестики-нолики";
 HWND hWndMain;
 WNDCLASS softwareMainClass;
 
+//Поля приложения
 static int** grid;
 static int n;
+
+//для fileMapping
 HANDLE hMapFile;
+int* pData;
 // ID для сообщения broadcast
 UINT WM_FIELD_CHANGED;
-int* pData;
+
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int nCmdShow) {
     resetData(cfg);
@@ -36,22 +40,19 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int nCmdSho
     }
 
     pData = (int*)MapViewOfFile(hMapFile, FILE_MAP_WRITE, 0, 0, sizeof(grid) * n * n);
-
     if (pData == NULL) {
         std::cout << "Не удалось получить указатель на представленную область памяти!" << std::endl;
         CloseHandle(hMapFile);
         return 1;
     }
 
-    grid = new int* [n];
-    for (int i = 0; i < n; i++) {
-        grid[i] = new int[n];
-        memset(grid[i], 0, sizeof(int) * n);
-    }
-
+    //Инициализация поля, если приложение уже запущено - инициализируем текущий прогресс
+    initGrid(grid, n);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            pData[i * n + j] = grid[i][j];
+            if (pData[i * n + j] != 0) {
+                grid[i][j] = pData[i * n + j];
+            }
         }
     }
 
@@ -81,6 +82,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int nCmdSho
     return 0;
 }
 
+
 WNDCLASS NewWindowClass(
     HBRUSH backgrondColor,
     HCURSOR cursor,
@@ -101,12 +103,14 @@ WNDCLASS NewWindowClass(
     return newWindowClass;
 }
 
+
 LRESULT CALLBACK SoftwareMainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
     
     static Rgb linesColor = { cfg.color_line.r, cfg.color_line.g, cfg.color_line.b };
     PAINTSTRUCT ps;
     
     if (msg == WM_FIELD_CHANGED) {
+        //initGrid(grid, n);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 grid[i][j] = pData[i * n + j];
@@ -117,147 +121,142 @@ LRESULT CALLBACK SoftwareMainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp
 
     switch (msg) {
 
-    case WM_CREATE: {
-        //initGrid(grid, n);
-        break;
-    }
+        case WM_CREATE: {
+            break;
+        }
     
+        case WM_PAINT: {
+            HDC hdc = BeginPaint(hWnd, &ps);
+            RECT clientRect;
+            GetClientRect(hWnd, &clientRect);
+            HBRUSH hBrush = CreateSolidBrush(RGB(linesColor.r, linesColor.g, linesColor.b));
 
+            int gridWidth = 0;
+            int gridHeight = 0;
 
-    case WM_PAINT: {
-        HDC hdc = BeginPaint(hWnd, &ps);
-        RECT clientRect;
-        GetClientRect(hWnd, &clientRect);
-        HBRUSH hBrush = CreateSolidBrush(RGB(linesColor.r, linesColor.g, linesColor.b));
+            getSizeOfGrid(clientRect, gridWidth, gridHeight, n);
 
-        int gridWidth = 0;
-        int gridHeight = 0;
+            //Отрисовка сетки, крестиков и ноликов
+            for (int x = 0; x < clientRect.right; x += gridWidth) {
+                for (int y = 0; y < clientRect.bottom; y += gridHeight) {
+                    int row = y / gridHeight;
+                    int col = x / gridWidth;
+                    //при n = 3 вылазит за границы массива
+                    if (row == n) {
+                        row = n-1;
+                    }
+                    if (col == n) {
+                        col = n-1;
+                    }
 
-        getSizeOfGrid(clientRect, gridWidth, gridHeight, n);
+                    RECT cellRect = { x, y, x + gridWidth, y + gridHeight };
+                    FrameRect(hdc, &cellRect, hBrush);
 
-        //Отрисовка сетки, крестиков и ноликов
-        for (int x = 0; x < clientRect.right; x += gridWidth) {
-            for (int y = 0; y < clientRect.bottom; y += gridHeight) {
-                int row = y / gridHeight;
-                int col = x / gridWidth;
-                //при n = 3 вылазит за границы массива
-                if (row == n) {
-                    row = n-1;
-                }
-                if (col == n) {
-                    col = n-1;
-                }
-
-                RECT cellRect = { x, y, x + gridWidth, y + gridHeight };
-                FrameRect(hdc, &cellRect, hBrush);
-
-                //Крестики
-                if (grid[row][col] == 1) {
-                    drawTic(x, y, hdc, gridWidth, gridHeight);
-                }
-                //Нолики
-                else if (grid[row][col] == 2) {
-                    drawTac(x, y, hdc, gridWidth, gridHeight);
+                    //Крестики
+                    if (grid[row][col] == 1) {
+                        drawTic(x, y, hdc, gridWidth, gridHeight);
+                    }
+                    //Нолики
+                    else if (grid[row][col] == 2) {
+                        drawTac(x, y, hdc, gridWidth, gridHeight);
+                    }
                 }
             }
+            EndPaint(hWnd, &ps);
+
+            //сохранение цвета линий в конфиг
+            cfg.color_line = linesColor;
+            setCurrentData(cfg);
+
+            DeleteObject(hBrush);
+            break;
         }
-        EndPaint(hWnd, &ps);
 
-        //сохранение цвета линий в конфиг
-        cfg.color_line = linesColor;
-        setCurrentData(cfg);
-
-        DeleteObject(hBrush);
-        break;
-    }
-
-    case WM_SIZE: {
-        InvalidateRect(hWnd, NULL, TRUE);
-        break;
-    }
+        case WM_SIZE: {
+            InvalidateRect(hWnd, NULL, TRUE);
+            break;
+        }
     
-    //Поставить крестик 
-    case WM_LBUTTONDOWN: {
-        int xPos = GET_X_LPARAM(lp);
-        int yPos = GET_Y_LPARAM(lp);
-        int gridWidth = 0;
-        int gridHeight = 0;
-        RECT clientRect;
-        GetClientRect(hWnd, &clientRect);
+        //Поставить крестик 
+        case WM_LBUTTONDOWN: {
+            int xPos = GET_X_LPARAM(lp);
+            int yPos = GET_Y_LPARAM(lp);
+            int gridWidth = 0;
+            int gridHeight = 0;
+            RECT clientRect;
+            GetClientRect(hWnd, &clientRect);
 
-        getSizeOfGrid(clientRect, gridWidth, gridHeight, n);
+            getSizeOfGrid(clientRect, gridWidth, gridHeight, n);
 
-        int row = yPos / gridHeight;
-        int col = xPos / gridWidth;
+            int row = yPos / gridHeight;
+            int col = xPos / gridWidth;
 
-        if (grid[row][col] == 0) {
-            pData[row * n + col] = 1;
-            //grid[row][col] = 1;
-            SendMessage(HWND_BROADCAST, WM_FIELD_CHANGED, 0, 0);
-            InvalidateRect(hWnd, NULL, TRUE);
+            if (grid[row][col] == 0) {
+                pData[row * n + col] = 1;
+                SendMessage(HWND_BROADCAST, WM_FIELD_CHANGED, 0, 0);
+                InvalidateRect(hWnd, NULL, TRUE);
+            }
+            break;
         }
-        break;
-    }
     
-    //Поставить нолик
-    case WM_RBUTTONDOWN: {
-        int xPos = GET_X_LPARAM(lp);
-        int yPos = GET_Y_LPARAM(lp);
-        int gridWidth = 0;
-        int gridHeight = 0;
-        RECT clientRect;
-        GetClientRect(hWnd, &clientRect);
+        //Поставить нолик
+        case WM_RBUTTONDOWN: {
+            int xPos = GET_X_LPARAM(lp);
+            int yPos = GET_Y_LPARAM(lp);
+            int gridWidth = 0;
+            int gridHeight = 0;
+            RECT clientRect;
+            GetClientRect(hWnd, &clientRect);
 
-        getSizeOfGrid(clientRect, gridWidth, gridHeight, n);
+            getSizeOfGrid(clientRect, gridWidth, gridHeight, n);
 
-        int row = yPos / gridHeight;
-        int col = xPos / gridWidth;
+            int row = yPos / gridHeight;
+            int col = xPos / gridWidth;
 
-        if (grid[row][col] == 0) {
-            pData[row * n + col] = 2;
-            //grid[row][col] = 2;            
-            SendMessage(HWND_BROADCAST, WM_FIELD_CHANGED, 0, 0);
+            if (grid[row][col] == 0) {
+                pData[row * n + col] = 2;          
+                SendMessage(HWND_BROADCAST, WM_FIELD_CHANGED, 0, 0);
+                InvalidateRect(hWnd, NULL, TRUE);
+            }
+
+            break;
+        }
+
+        case WM_KEYDOWN: {
+            if (wp == VK_RETURN) {
+                changeBackgroundColor(hWnd, cfg);
+                InvalidateRect(hWnd, NULL, TRUE);
+            }
+            else if (wp == VK_ESCAPE) {
+                DestroyWindow(hWnd);
+            }
+            else if ((GetKeyState(VK_CONTROL) & 0x8000) && (wp == 'Q' || wp == 'q')) {
+                DestroyWindow(hWnd);
+            }
+            else if ((GetKeyState(VK_SHIFT) & 0x8000) && (wp == 'C' || wp == 'c')) {
+                runNotepad();
+            }
+
+            break;
+        }
+
+        case WM_MOUSEWHEEL: {
+            changeLineColors(linesColor);
             InvalidateRect(hWnd, NULL, TRUE);
+            break;
         }
 
-        break;
-    }
+        case WM_DESTROY: {
+            RECT clientRect;
+            GetWindowRect(hWnd, &clientRect);
+            saveData(clientRect);
 
-    case WM_KEYDOWN: {
-        if (wp == VK_RETURN) {
-            changeBackgroundColor(hWnd, cfg);
-            InvalidateRect(hWnd, NULL, TRUE);
-        }
-        else if (wp == VK_ESCAPE) {
-            DestroyWindow(hWnd);
-        }
-        else if ((GetKeyState(VK_CONTROL) & 0x8000) && (wp == 'Q' || wp == 'q')) {
-            DestroyWindow(hWnd);
-        }
-        else if ((GetKeyState(VK_SHIFT) & 0x8000) && (wp == 'C' || wp == 'c')) {
-            runNotepad();
+            PostQuitMessage(0);
+            return 0; 
         }
 
-        break;
-    }
-
-    case WM_MOUSEWHEEL: {
-        changeLineColors(linesColor);
-        InvalidateRect(hWnd, NULL, TRUE);
-        break;
-    }
-
-    case WM_DESTROY: {
-        RECT clientRect;
-        GetWindowRect(hWnd, &clientRect);
-        saveData(clientRect);
-
-        PostQuitMessage(0);
-        return 0; 
-    }
-
-    default:
-        return DefWindowProc(hWnd, msg, wp, lp);
+        default:
+            return DefWindowProc(hWnd, msg, wp, lp);
     }
     
     DeleteObject(hWndMain);
